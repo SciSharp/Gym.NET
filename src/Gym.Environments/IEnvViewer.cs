6cd4dc4ad02b29b3541d2521f9c7dbbf3052a747
Eli Belash <elibelash@gmail.com>
2019-10-28 21:12:57 +0200

    Added documentation to IEnvViewer

--- src/Gym.Environments/IEnvViewer.cs
+++ src/Gym.Environments/IEnvViewer.cs
@@ -4,7 +4,15 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace Gym.Environments {
     public interface IEnvViewer : IDisposable {
+        /// <summary>
+        ///     Render the given <paramref name="img"/>.
+        /// </summary>
+        /// <param name="img">The image to render.</param>
         void Render(Image<Rgba32> img);
+
+        /// <summary>
+        ///     Close the rendering window.
+        /// </summary>
         void Close();
     }
 }
