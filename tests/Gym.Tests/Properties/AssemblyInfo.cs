732e62097de0ae627e930f50cc971d63a8be971d
Eli Belash <elibelash@gmail.com>
2019-11-05 12:19:56 +0200

    Gym.Tests: Changed target to netcoreapp3.0

--- tests/Gym.Tests/Properties/AssemblyInfo.cs
+++ tests/Gym.Tests/Properties/AssemblyInfo.cs
@@ -2,19 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Gym.Tests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gym.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Eli Belash 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("03dc11d1-c5bf-4e1e-b96d-2b0eefae8f70")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
